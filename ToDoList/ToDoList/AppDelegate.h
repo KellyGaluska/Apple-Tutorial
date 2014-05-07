c6e22c53dc89f9151ea81221fb6e59fd9aac381b
Kelly Galuska <klgaluska@crimson.ua.edu>
2014-05-07 10:42:23 -0500

    no message

---
+++ ToDoList/ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Student on 5/7/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
