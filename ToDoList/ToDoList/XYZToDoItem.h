e4b2a118e1a3a2457ff18a2c5f24935cea394f03
Kelly Galuska <klgaluska@crimson.ua.edu>
2014-05-08 11:06:28 -0500

    no message

---
+++ ToDoList/ToDoList/XYZToDoItem.h
@@ -0,0 +1,13 @@
+//
+//  XYZToDoItem.h
+//  ToDoList
+//
+//  Created by Student on 5/8/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XYZToDoItem : NSObject
+
+@end
---
+++ ToDoList/ToDoList/XYZToDoItem.m
@@ -0,0 +1,13 @@
+//
+//  XYZToDoItem.m
+//  ToDoList
+//
+//  Created by Student on 5/8/14.
+//
+//
+
+#import "XYZToDoItem.h"
+
+@implementation XYZToDoItem
+
+@end
