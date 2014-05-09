4d2558ea8581cd1b605e18ebf0371b85ec0291c9
Kelly Galuska <klgaluska@crimson.ua.edu>
2014-05-09 11:48:50 -0500

    no message

--- ToDoList/ToDoList/XYZAboutViewController.m
+++ ToDoList/ToDoList/XYZAboutViewController.m
@@ -29,13 +29,13 @@
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor whiteColor];
     
-    UIButton *githubButton = [[UIButton alloc]initWithFrame:CGRectMake(10,75,100,30)];
+    UIButton *githubButton = [[UIButton alloc]initWithFrame:CGRectMake(10,120,100,30)];
     githubButton.backgroundColor = [UIColor redColor];
     [githubButton setTitle:@"My Github" forState:UIControlStateNormal];
     [githubButton addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:githubButton];
     self.title = @"About";
-    UILabel *label1 = [[UILabel alloc] initWithFrame:CGRectMake(10, 100, 200, 40)];
+    UILabel *label1 = [[UILabel alloc] initWithFrame:CGRectMake(10, 75, 200, 40)];
     [label1 setTextColor:[UIColor blackColor]];
     [label1 setText:@"Author: Kelly Galuska"];
     [[self view] addSubview:label1];
